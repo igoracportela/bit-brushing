a928cf1056e8e681ee4f18e0317454123c817c93
Igor Portela <85773564+igoracportela@users.noreply.github.com>
2022-10-28 10:42:15 -0300

    Update observation-station.sql

--- sql/observation-station.sql
+++ sql/observation-station.sql
@@ -1,6 +1,12 @@
-# Challenge: https://www.hackerrank.com/challenges/weather-observation-station-16/problem
 #
 # Author: Igor A. C. Portela | Copyright(c) 2022. All rights reserved.
 # github: @igoracportela
 #
+# Challenge: https://www.hackerrank.com/challenges/weather-observation-station-16/problem
 SELECT ROUND(LAT_N, 4) FROM station WHERE LAT_N >= 38.770 ORDER BY LAT_N ASC LIMIT 1
+
+# Challenge: https://www.hackerrank.com/challenges/weather-observation-station-17/problem
+SELECT ROUND(LONG_W, 4) FROM station WHERE LAT_N >= 38.7780 ORDER BY LAT_N ASC LIMIT 1
+
+# Challenge: https://www.hackerrank.com/challenges/weather-observation-station-18/problem
+SELECT ROUND(((MAX(LAT_N)-MIN(LAT_N))+(MAX(LONG_W)-MIN(LONG_W))),4) FROM station
