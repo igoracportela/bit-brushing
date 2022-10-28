4f1ed8657c374d77917c8c94429896b22cc543a3
Igor Portela <85773564+igoracportela@users.noreply.github.com>
2022-10-28 10:49:26 -0300

    Update observation-station.sql

--- sql/observation-station.sql
+++ sql/observation-station.sql
@@ -9,4 +9,15 @@ SELECT ROUND(LAT_N, 4) FROM station WHERE LAT_N >= 38.770 ORDER BY LAT_N ASC LIM
 SELECT ROUND(LONG_W, 4) FROM station WHERE LAT_N >= 38.7780 ORDER BY LAT_N ASC LIMIT 1
 
 # Challenge: https://www.hackerrank.com/challenges/weather-observation-station-18/problem
-SELECT ROUND(((MAX(LAT_N)-MIN(LAT_N))+(MAX(LONG_W)-MIN(LONG_W))),4) FROM station
+SELECT ROUND(
+  (
+    (MAX(LAT_N)-MIN(LAT_N))+(MAX(LONG_W)-MIN(LONG_W))
+  ),4)
+FROM station
+
+# Challenge: https://www.hackerrank.com/challenges/weather-observation-station-19/problem
+SELECT ROUND(
+  POWER(
+    POWER(MAX(LAT_N)-MIN(LAT_N),2) + POWER(MAX(LONG_W)-MIN(LONG_W ),2),1/2
+  ),4)
+FROM station
