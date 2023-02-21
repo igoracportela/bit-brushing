260ae95eb32433015d4e7d2d9cc1715e2747f94f
Igor Portela <85773564+igoracportela@users.noreply.github.com>
2023-02-21 15:32:21 -0300

    Update with more sort algorithm

--- ruby/bigO_quicksort.rb
+++ ruby/bigO_quicksort.rb
@@ -2,7 +2,8 @@
 # Author - Igor A. C. Portela | Copyright(c) 2022. All rights reserved.
 # github - @igoracportela
 #
-
+# Quicksort algorithm 
+#
 def quicksort(arr)
   return arr if arr.size <= 1
 
@@ -28,3 +29,29 @@ end
 arr = [7, 2, 9, 4, 1, 8, 5, 6, 3]
 sorted_arr = quicksort(arr)
 p sorted_arr.inspect
+
+# ---------------------
+#
+# BubbleSort Algorithm 
+#
+def bubble_sort(array)
+  n = array.length
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+end
+
+# example
+arr = [5,2,4,3,1,9,7,6,8,10]
+p bubble_sort(arr)
