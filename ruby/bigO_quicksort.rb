0f7e3d4d802b949ceee3bf91aa6099ee6ef73b60
Igor Portela <85773564+igoracportela@users.noreply.github.com>
2023-02-21 14:38:44 -0300

    Update bigO_quicksort.rb

--- ruby/bigO_quicksort.rb
+++ ruby/bigO_quicksort.rb
@@ -3,36 +3,28 @@
 # github - @igoracportela
 #
 
-def quick_sort_algo(array, first_elem, last_elem)
- if first_elem < last_elem
-   index = part(array, first_elem, last_elem)
-   quick_sort_algo(array, first_elem, index-1)
-   quick_sort_algo(array, index+1, last_elem)
- end
- 
- array.join('')
-end
- 
-def part(array, first_elem, last_elem)
- pivot = array[last_elem]
- index = first_elem
- i = first_elem
- while i < last_elem
-   if array[i].to_i <= pivot.to_i
-     temporary = array[i]
-     array[i] = array[index]
-     array[index] = temporary
-     index += 1
-   end
-   i += 1
- end
- temporary = array[index]
- array[index] = pivot
- array[last_elem] = temporary
- 
- index
+def quicksort(arr)
+  return arr if arr.size <= 1
+
+  # Pick the pivot element
+  pivot = arr.last
+
+  # Partition the array
+  left = []
+  right = []
+  arr[0...-1].each do |x|
+    if x < pivot
+      left << x
+    else
+      right << x
+    end
+  end
+
+  # Recursively
+  quicksort(left) + [pivot] + quicksort(right)
 end
 
-# Call the method
-int = [4,3,5,1,2]
-puts quick_sort_algo(int, 0, 4)
+# Usage
+arr = [7, 2, 9, 4, 1, 8, 5, 6, 3]
+sorted_arr = quicksort(arr)
+p sorted_arr.inspect
